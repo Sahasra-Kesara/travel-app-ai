bd354c7c3d5a2d2f5613dae45c5f3d88fab6d2ff
StackNet <121740972+Sahasra-Kesara@users.noreply.github.com>
2026-01-13 09:49:44 +0530

    Added all

--- app/services/route_service.py
+++ app/services/route_service.py
@@ -4,26 +4,11 @@ def get_route(start_lat, start_lon, end_lat, end_lon):
     url = (
         f"http://router.project-osrm.org/route/v1/driving/"
         f"{start_lon},{start_lat};{end_lon},{end_lat}"
-        "?overview=full&geometries=geojson&steps=true"
+        "?overview=full&geometries=geojson"
     )
 
     response = requests.get(url)
     data = response.json()
 
-    route = data["routes"][0]
-
-    coordinates = route["geometry"]["coordinates"]
-    steps = []
-
-    for leg in route["legs"]:
-        for step in leg["steps"]:
-            steps.append({
-                "instruction": step["maneuver"]["instruction"],
-                "distance": step["distance"],
-                "duration": step["duration"]
-            })
-
-    return {
-        "coordinates": coordinates,
-        "steps": steps
-    }
+    # coordinates = [[lon, lat], ...]
+    return data["routes"][0]["geometry"]["coordinates"]
