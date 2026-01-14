8a998827f5b8f976ed074c1576832acaf8b79376
StackNet <121740972+Sahasra-Kesara@users.noreply.github.com>
2026-01-14 09:55:56 +0530

    Added multi route service

---
+++ app/services/multi_route_service.py
@@ -0,0 +1,13 @@
+def build_route(segment):
+    mode = segment["mode"]
+
+    if mode == "train":
+        return get_train_route(segment["from"], segment["to"])
+
+    if mode == "bus":
+        return get_osrm_route(segment["from"], segment["to"], "normal")
+
+    if mode == "highway_car":
+        return get_osrm_route(segment["from"], segment["to"], "highway")
+
+    return get_osrm_route(segment["from"], segment["to"], "normal")
